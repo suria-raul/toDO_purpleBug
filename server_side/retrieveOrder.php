2c470690e7d7585c015d25c6eebc1d7b988ad382
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-04-04 18:02:23 +0800

    successfully modified retrieveOrder.php

--- server_side/retrieveOrder.php
+++ server_side/retrieveOrder.php
@@ -2,24 +2,6 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-include 'connection.php';
+use App\Models\Order;
 
-use App\Database;
-
-$data = [];
-
-$query = "SELECT * FROM orders";
-
-if (!$result = mysqli_query((new Database())->connect(), $query)) {
-    exit(mysqli_error((new Database())->connect()));
-}
-
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = $row;
-    }
-} else {
-    $data[] = 'Records not found!';
-}
-
-echo json_encode($data);
+(new Order())->all();
--- src/Models/Order.php
+++ src/Models/Order.php
@@ -1,1 +1,47 @@
 <?php
+
+namespace App\Models;
+
+use App\Database;
+
+class Order
+{
+    protected $result;
+
+    protected Database $database;
+
+    public array $data = [];
+
+    public function __construct()
+    {
+        $this->database = new Database();
+        $this->result = mysqli_query($this->database->connect(), "SELECT * FROM orders");
+    }
+
+    private function isConnected()
+    {
+        if (!$this->result) {
+            exit(mysqli_error($this->database->connect()));
+        }
+
+        return true;
+    }
+
+    private function hasData()
+    {
+        return mysqli_num_rows($this->result) > 0;
+    }
+
+    public function all()
+    {
+        if ($this->isConnected() && $this->hasData()) {
+            while ($row = mysqli_fetch_assoc($this->result)) {
+                $this->data[] = $row;
+            }
+        } else {
+            $this->data[] = 'No Orders Found!';
+        }
+
+        echo json_encode($this->data);
+    }
+}
